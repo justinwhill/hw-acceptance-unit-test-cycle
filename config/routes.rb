ecbb2556109f643127071fbbc898b3742c522d03
justinwhill <justinwhill@outlook.com>
2018-03-18 12:23:28 +0000

    route bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
+  root 'movies#index'
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  # root :to => redirect('/movies')
   get 'same_director/:title' => 'movies#find', as: :find_same_director
 end
